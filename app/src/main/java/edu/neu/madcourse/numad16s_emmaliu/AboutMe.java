695323b820f0d7c0d2a22ade7fa24b2fa3022290
EmmaL <liu.yuji@husky.neu.edu>
2017-01-19 02:38:09 -0800

    finish main page

--- app/src/main/java/edu/neu/madcourse/numad16s_emmaliu/AboutMe.java
+++ app/src/main/java/edu/neu/madcourse/numad16s_emmaliu/AboutMe.java
@@ -1,16 +1,15 @@
 package edu.neu.madcourse.numad16s_emmaliu;
 
-import android.content.DialogInterface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 
-public class MainActivity extends AppCompatActivity {
+public class AboutMe extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about);
     }
 
 
