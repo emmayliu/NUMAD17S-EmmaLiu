e3e6ee491767308a0d040f0f2564064f8f8c7f4e
EmmaL <liu.yuji@husky.neu.edu>
2017-01-19 13:28:48 -0800

    change to unique icon

--- app/src/main/java/edu/neu/madcourse/numad16s_emmaliu/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad16s_emmaliu/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-
     public void getError(View v) {
         throw new RuntimeException("This is a crash");
     }
