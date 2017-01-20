61c2d2174c778a393154fa7595d2fc0d911f7c05
EmmaL <liu.yuji@husky.neu.edu>
2017-01-20 00:56:52 -0800

    fix app name conflict

--- app/src/main/java/edu/neu/madcourse/numad16s_emmaliu/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad16s_emmaliu/MainActivity.java
@@ -13,16 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void about_button_Click(View view) {
         Intent intent = new Intent(MainActivity.this, AboutMe.class);
         startActivity(intent);
     }
-    
+
     public void getError(View v) {
         throw new RuntimeException("This is a crash");
     }
-
 
 }
