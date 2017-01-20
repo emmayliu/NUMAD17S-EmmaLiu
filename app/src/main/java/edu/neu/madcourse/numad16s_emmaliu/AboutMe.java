3c93ebe777ac03ce995d029d602bb92c078051b1
EmmaL <liu.yuji@husky.neu.edu>
2017-01-20 14:13:25 -0800

    get IMID dynamatically

--- app/src/main/java/edu/neu/madcourse/numad16s_emmaliu/AboutMe.java
+++ app/src/main/java/edu/neu/madcourse/numad16s_emmaliu/AboutMe.java
@@ -1,16 +1,25 @@
 package edu.neu.madcourse.numad16s_emmaliu;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
 
 
 public class AboutMe extends AppCompatActivity {
+    public String device_id;
+    public TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-    }
 
+        TelephonyManager tm = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        device_id = tm.getDeviceId();
+        textView = (TextView) findViewById(R.id.phoneId);
+        textView.setText(device_id);
+    }
 
 }
