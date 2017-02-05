7973d48492d6fd952f086c9e9be18439c39c5d1a
EmmaL <liu.yuji@husky.neu.edu>
2017-02-05 01:39:22 -0800

    Complete build trie on small wordlist

--- app/src/main/java/edu/neu/madcourse/numad17s_emmaliu/Dictionary.java
+++ app/src/main/java/edu/neu/madcourse/numad17s_emmaliu/Dictionary.java
@@ -1,6 +1,8 @@
 package edu.neu.madcourse.numad17s_emmaliu;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.renderscript.ScriptGroup;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,10 +11,16 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 
-public class Dictionary extends AppCompatActivity {
+
+public class Dictionary extends AppCompatActivity  {
+    private static final String TAG = "Test File existense";
     ArrayList<String> words = new ArrayList<>();
 
     @Override
@@ -29,6 +37,51 @@ public class Dictionary extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.word_list);
         listView.setAdapter(adapter);
 
+        //test if json file already exist
+
+        AssetManager manager = getResources().getAssets();
+        InputStream is = null;
+        try {
+            is = manager.open("shortWordlist.txt");
+        } catch (IOException ex) {
+            Log.e(TAG, "cannot get file");
+
+        } finally {
+            if (is != null) {
+                Log.i(TAG, "Wordlist file is here");
+            }
+        }
+
+        try {
+            readData();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public void readData() throws IOException {
+        String data = "";
+        try {
+            InputStream is = getAssets().open("shortWordlist.txt");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            data = new String(buffer);
+            Log.e(data, "This is words");
+            String[] words = data.split("\n");
+
+            Trie trie = new Trie();
+            for (String w : words) {
+                trie.insert(w);
+            }
+            Log.i(TAG, "trie build complete");
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
 
---
+++ app/src/main/java/edu/neu/madcourse/numad17s_emmaliu/Trie.java
@@ -0,0 +1,96 @@
+package edu.neu.madcourse.numad17s_emmaliu;
+
+/**
+ * Created by emma on 2/4/17.
+ */
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+class Node {
+
+    //assume we only have 26 character based on provided wordlist
+    private final int C = 26;
+    private Node[] children;
+    private boolean isEnd;
+
+    //constructor
+    public Node() {
+        children = new Node[C];
+    }
+
+    public void setEnd() {
+        isEnd = true;
+    }
+
+    public boolean isEnd() {
+        return isEnd;
+    }
+
+    public boolean contains (char c) {
+        if (children[c - 'a'] != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void set(char c, Node node) {
+        children[c - 'a'] = node;
+    }
+
+    public Node get (char c) {
+        return children[c - 'a'];
+    }
+}
+
+
+public class Trie {
+    private Node root;
+
+    //constructor
+    public Trie() {
+        root = new Node();
+    }
+
+    //1. Insert a word into trie
+
+    public void insert(String word) {
+        Node curNode = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!curNode.contains(c)) {
+                curNode.set(c, new Node());
+            }
+            curNode = curNode.get(c);
+        }
+        curNode.setEnd();
+    }
+
+    //2. Search Prefix
+    private Node searchPrefix(String word) {
+        Node curNode = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (curNode.contains(c)) {
+                curNode = curNode.get(c);
+            } else {
+                return null;
+            }
+        }
+        return curNode;
+    }
+
+    //3. Search word
+
+    public boolean search(String word) {
+        Node node = searchPrefix(word);
+        if (node != null && node.isEnd()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
+
